805d2d033d6306887039b09405f2ad61b571ef01
Joost Steltenpool <jcsteltenpool@users.noreply.github.com>
2022-10-18 14:53:27 +0200

    Set width=100% on backgr-img

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-let mobileMenu = document.getElementsByClassName("mobile-menu")[0];
-let showMenuButton = document.getElementsByClassName("js-show-menu")[0];
-let hideMenuButton = document.getElementsByClassName("menu-button-close")[0];
-let hideMenu_array = document.getElementsByClassName("js-hide-menu");
+    let mobileMenu = document.getElementsByClassName("mobile-menu")[0];
+    let showMenuButton = document.getElementsByClassName("js-show-menu")[0];
+    let hideMenuButton = document.getElementsByClassName("menu-button-close")[0];
+    let hideMenu_array = document.getElementsByClassName("js-hide-menu");
+    
+    for (let i = 0; i < hideMenu_array.length; i++) {
+        hideMenu_array[i].addEventListener('click', hideMenu)
+    };
+    
+    showMenuButton.addEventListener('click', showMenu);
+    function showMenu() {
+        mobileMenu.style.display = 'block';
+        hideMenuButton.hidden = false;
+        showMenuButton.hidden = true;
+    };
+    
+    function hideMenu() {
+        mobileMenu.style.display = 'none';
+        hideMenuButton.hidden = true;
+        showMenuButton.hidden = false;
+    };
+});
 
-for (let i = 0; i < hideMenu_array.length; i++) {
-    hideMenu_array[i].addEventListener('click', hideMenu)
-};
-
-
-showMenuButton.addEventListener('click', showMenu);
-function showMenu() {
-    mobileMenu.style.display = 'block';
-    hideMenuButton.hidden = false;
-    showMenuButton.hidden = true;
-};
-
-function hideMenu() {
-    mobileMenu.style.display = 'none';
-    hideMenuButton.hidden = true;
-    showMenuButton.hidden = false;
-};
 
 
 
